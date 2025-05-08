39fa123bea0eeda24c5b1dac07bbbf41bc299d43
Luis Baquedano <lfbcordova@gmail.com>
2025-05-08 06:59:12 -0500

    Corregir formato de script por cuarta vez

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function saludar () {
-  return 'Saludos terricolas, I am dev';
-}
-
-const numerosPares = [2, 4, 6, 8];
-
-console.log(saludar());
-console.log(numerosPares);
+    return 'Saludos terricolas, I am dev'
+  }
+  
+  const numerosPares = [2, 4, 6, 8]
+  
+  console.log(saludar())
+  console.log(numerosPares)
+  
